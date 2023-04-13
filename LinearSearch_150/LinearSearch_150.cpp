d417601086411f027a0c4ac8f22a99003665c4f0
Rangga857 <azharrangga28@gmail.com>
2023-04-13 15:05:24 +0700

    Algoritma Linearsearch

--- LinearSearch_150/LinearSearch_150.cpp
+++ LinearSearch_150/LinearSearch_150.cpp
@@ -52,5 +52,10 @@ void Linearsearch()
 		}
 
 		if (i == n)											// Langkah no 5 Algoritma Linearsearch
-	}
+			cout << "\n" << item << " not found in the arrayy\n ";
+		cout << "\nNumber of comparisons: " << ctr << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'y'));
 }
